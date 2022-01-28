5bdc0e5e6ad58993e2d632d692c06176cfc78921
StarrySky <Your_email>
2022-01-28 21:28:34 +0800

    BaseService extends Component

--- services/BaseService.php
+++ services/BaseService.php
@@ -2,11 +2,13 @@
 
 namespace app\services;
 
+use yii\base\Component;
+
 /**
  * 服务层 父类
  * 可以放一些公用的操作model的方法、属性
  */
-class BaseService
+class BaseService extends Component
 {
     public $model;
 
